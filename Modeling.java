3ad92448a2a75511064d3ccd40a93f1065128856
FRC6302 <61292082+FRC6302@users.noreply.github.com>
2022-01-21 17:20:17 -0500

    got it running

--- Modeling.java
+++ Modeling.java
@@ -1,3 +1,18 @@
 public class Modeling {
-    double vx = 10;
+    public static void main(String[] args) {
+        final double airDensity = Math.cos(5);
+        final double crossSectionArea = 3;
+        final double dragCoef = 0.23;
+        final double liftCoef = 0.1;
+
+
+        final double dragC = 0.5 * airDensity * crossSectionArea * dragCoef;
+        final double liftC = 0.5 * airDensity * crossSectionArea * liftCoef;
+
+    
+    }
+
+    
+
+    
 }
